0f39f31c7f5cbbe1ba156f8d84b1d71bc6914058
Ahmed Şeref GÜNEYSU <ahmed.guneysu@generalmobile.com>
2019-07-30 06:44:56 +0300

    webpack based theme development

---
+++ themes/guneysu-blog/src/index.js
@@ -0,0 +1,72 @@
+import './app.css'
+import './css/styles.css'
+import './css/pygments/autumn.css'
+
+/* External Library Includes */
+import 'tachyons/src/_normalize.css';
+
+
+/* Modules */
+import 'tachyons/src/_box-sizing.css';
+// import 'tachyons/src/_aspect-ratios.css'
+import 'tachyons/src/_images.css'
+// import 'tachyons/src/_background-size.css'
+// import 'tachyons/src/_background-position.css'
+// import 'tachyons/src/_outlines.css'
+import 'tachyons/src/_borders.css'
+import 'tachyons/src/_border-colors.css'
+import 'tachyons/src/_border-radius.css'
+// import 'tachyons/src/_border-style.css'
+// import 'tachyons/src/_border-widths.css'
+// import 'tachyons/src/_box-shadow.css'
+// import 'tachyons/src/_code.css'
+// import 'tachyons/src/_coordinates.css'
+// import 'tachyons/src/_clears.css'
+import 'tachyons/src/_display.css'
+import 'tachyons/src/_flexbox.css'
+// import 'tachyons/src/_floats.css'
+import 'tachyons/src/_font-family.css'
+// import 'tachyons/src/_font-style.css'
+// import 'tachyons/src/_font-weight.css'
+// import 'tachyons/src/_forms.css'
+// import 'tachyons/src/_heights.css'
+// import 'tachyons/src/_letter-spacing.css'
+import 'tachyons/src/_line-height.css'
+import 'tachyons/src/_links.css'
+// import 'tachyons/src/_lists.css'
+import 'tachyons/src/_max-widths.css'
+import 'tachyons/src/_widths.css'
+// import 'tachyons/src/_overflow.css'
+// import 'tachyons/src/_position.css'
+// import 'tachyons/src/_opacity.css'
+// import 'tachyons/src/_rotations.css'
+import 'tachyons/src/_skins.css'
+import 'tachyons/src/_skins-pseudo.css'
+import 'tachyons/src/_spacing.css'
+// import 'tachyons/src/_negative-margins.css'
+// import 'tachyons/src/_tables.css'
+// import 'tachyons/src/_text-decoration.css'
+import 'tachyons/src/_text-align.css'
+// import 'tachyons/src/_text-transform.css'
+import 'tachyons/src/_type-scale.css'
+// import 'tachyons/src/_typography.css'
+import 'tachyons/src/_utilities.css'
+// import 'tachyons/src/_visibility.css'
+// import 'tachyons/src/_white-space.css'
+// import 'tachyons/src/_vertical-align.css'
+import 'tachyons/src/_hovers.css'
+// import 'tachyons/src/_z-index.css'
+// import 'tachyons/src/_nested.css'
+// import 'tachyons/src/_styles.css'
+
+/* Variables */
+/* Importing here will allow you to override any variables in the modules */
+import 'tachyons/src/_colors.css'
+import 'tachyons/src/_media-queries.css'
+
+/* Debugging */
+// import 'tachyons/src/_debug-children.css'
+// import 'tachyons/src/_debug-grid.css'
+
+/* Uncomment out the line below to help debug layout issues */
+/* import 'tachyons/src/_debug.css' */
