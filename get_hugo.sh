9e9782f293562a64ab7cb5260f942d997ca94525
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2019-10-20 14:16:18 +0300

    post page updated
    - author
    - last updated date
    - write a review button
    - git info
    hugo deb download script added
    - git and comment icons added

---
+++ get_hugo.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+set -ex
+
+VERSION="$(cat HUGO_VERSION)"
+
+DEB_URL="https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_extended_${VERSION}_Linux-64bit.deb"
+
+wget ${DEB_URL} -O ~/Downloads/hugo.deb
