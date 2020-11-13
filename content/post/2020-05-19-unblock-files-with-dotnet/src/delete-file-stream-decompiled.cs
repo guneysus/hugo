66e770900c3efa965a1a4eed1475bd6870f12039
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-11-13 10:10:54 +0300

    hotfix

--- content/post/2020-05-19-unblock-files-with-dotnet/src/delete-file-stream-decompiled.cs
+++ content/post/2020-05-19-unblock-files-with-dotnet/src/delete-file-stream-decompiled.cs
@@ -1,20 +1,19 @@
-// System.Management.Automation.Internal.AlternateDataStreamUtilities
 using System.IO;
 
 internal static void DeleteFileStream(string path, string streamName)
 {
- if (path == null)
- {
-  throw new ArgumentNullException("path");
- }
- if (streamName == null)
- {
-  throw new ArgumentNullException("streamName");
- }
- string text = streamName.Trim();
- if (text.IndexOf(':') != 0)
- {
-  text = ":" + text;
- }
- File.Delete(path + text);
+    if (path == null)
+    {
+        throw new ArgumentNullException("path");
+    }
+    if (streamName == null)
+    {
+        throw new ArgumentNullException("streamName");
+    }
+    string text = streamName.Trim();
+    if (text.IndexOf(':') != 0)
+    {
+        text = ":" + text;
+    }
+    File.Delete(path + text);
 }
